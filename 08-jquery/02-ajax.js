76fb12da414878ebd38870d4afd8def809e073fd
abdullahi_megenow <amegenow@pdx.edu>
2025-11-23 21:43:23 -0800

    Week 8 Lab

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,21 +1,70 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+$("document").ready(() => {
+  const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+  const addBookToDOM = (item) => {
+    console.log(item.name);
+
+    $("#books").append(
+      $("<div>")
+        .css({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        })
+        .append($("<h3>").text(item.authors[0]))
+        .append($("<p>").text(item.released.substr(0, 4)))
+        .append($("<p>").text(`${item.numberOfPages} pages`))
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        console.log(data);
+        data.forEach((item) => {
+          //  console.log(item.name);
+          addBookToDOM(item);
+        });
+      },
+
+      error: (error) => {
+        console.log(error);
+        $("#books")
+          .append($("<div>"))
+          .text(`An error occured. Please try again.`);
+      },
+
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
+});
+
+//const url = 'https://anapioficeandfire.com/api/books/';
+
+/*
+const app = document.querySelector("#books");
 app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+const loading = document.querySelector("#loading");
 
 const addBookToDOM = (item) => {
   console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+  let element = document.createElement("div");
+  let title = document.createElement("h4");
+  let author = document.createElement("p");
+  let published = document.createElement("p");
+  let pages = document.createElement("p");
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
 
   title.textContent = item.name;
   author.textContent = `by ${item.authors[0]}`;
@@ -40,7 +89,7 @@ const fetchData = (url) => {
     })
     .catch((error) => {
       console.log(error);
-      let li = document.createElement('li');
+      let li = document.createElement("li");
       li.textContent = `An error occured. Please try again.`;
       app.append(li);
     })
@@ -48,5 +97,5 @@ const fetchData = (url) => {
       app.removeChild(loading);
     });
 };
-
-fetchData(url);
+*/
+//fetchData(url);
